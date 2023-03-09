442c57ddf602e17223e7938be70375d5b9907416
Md Rafi Akhtar <alimdrafi@gmail.com>
2023-03-09 20:36:16 +0530

    Added useSelector and useDispatch for showing counter value and changing its count

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,25 @@
-import classes from './Counter.module.css';
+import { useSelector, useDispatch } from "react-redux";
+
+import classes from "./Counter.module.css";
 
 const Counter = () => {
+  const counter = useSelector((state) => state.counter);
+  const dispatchFn = useDispatch();
+
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={() => dispatchFn({ type: "INCREMENT" })}>
+          Increment
+        </button>
+        <button onClick={() => dispatchFn({ type: "DECREMENT" })}>
+          Decrement
+        </button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
